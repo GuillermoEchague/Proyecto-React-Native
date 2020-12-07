07a3bea25fa5001e0e7fc9dcb566886a1bcf453d
Guillermo Echague <guillermo.echaguea@gmail.com>
2020-12-07 01:18:50 -0300

    Navigation y cambio de color ok

--- App.js
+++ App.js
@@ -1,12 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, {useState,useMemo} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -16,99 +8,63 @@ import {
   StatusBar,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Provider as PaperProvider, 
+  DarkTheme as DarkThemePaper, 
+  DefaultTheme as DefaultThemePaper,
+  DefaultTheme,} 
+  from 'react-native-paper';
+import {NavigationContainer, 
+  DarkTheme as DarkThemeNavigation, 
+  DefaultTheme as DefaultThemeNavigation,} 
+  from "@react-navigation/native";
+import Navigation from "./src/navigation/Navigation";
+import PreferencesContext from "./src/context/PreferencesContext";
+
+
+
 
 const App: () => React$Node = () => {
+  const [theme, setTheme] = useState("dark");
+
+  DefaultThemePaper.colors.primary = "#1ae1f2";
+  DarkThemePaper.colors.primary="#1ae1f2";
+  DarkThemePaper.colors.accent ="#1ae1f2";
+
+  DarkThemeNavigation.colors.background="#192734";
+  DarkThemeNavigation.colors.card = "#15212b";
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "Light":"dark");
+  }
+
+  const preference = useMemo(
+    () => ({
+      toggleTheme,
+      theme,
+    }),
+    [theme],
+  );
+    
+
+
+
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+    <PreferencesContext.Provider value={preference}>
+    <PaperProvider theme={theme === "dark"? DarkThemePaper : DefaultThemePaper}>
+      <StatusBar barStyle={theme === "dark"? "light-content" : "dark-content"}/>
+        <NavigationContainer theme ={theme === "dark"? DarkThemeNavigation : DefaultThemeNavigation}>
+     <Navigation/>
+      </NavigationContainer>
+
+    </PaperProvider>
+    </PreferencesContext.Provider>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  
 });
 
 export default App;
